3578e3c50e68eab50356f888c48ade8dfc930d83
controlol <laluckyluc7@gmail.com>
2021-05-08 09:23:21 +0200

    fix undefined variables

--- api/friend.php
+++ api/friend.php
@@ -78,6 +78,7 @@ if ($method == "GET") {
 }
 
 function fetchUserFriends(&$arr, $uid) {
+  global $conn;
   // first get friends in one direction
   $sql = "SELECT u.user_id, u.avatar, u.username FROM friends f INNER JOIN users u ON f.user_id2 = u.user_id WHERE user_id1 = $uid;";
   if (!$result = $conn->query($sql)) echoSQLerror($sql, $conn->error);
@@ -87,16 +88,17 @@ function fetchUserFriends(&$arr, $uid) {
 
 function appendUserInfo(&$arr, $rows, $uid) {
   while ($r = $rows->fetch_assoc()) {
-    $user_id = $userinfo["user_id"];
+    global $conn;
+    $user_id = $r["user_id"];
 
     // check if both users added each other
     $sql = "SELECT user_id1 FROM friends WHERE ( user_id1 = $user_id AND user_id2 = $uid );";
     if (!$result = $conn->query($sql)) echoSQLerror($sql, $conn->error);
     $pending = $result->num_rows == 1 ? false : true;
 
-    $avatar = $userinfo["avatar"];
-    $username = $userinfo["username"];
-    array_push($friend_arr, array("id" => $user_id, "avatar" => base64_encode($avatar), "username" => $username, "pending" => $pending));
+    $avatar = $r["avatar"];
+    $username = $r["username"];
+    array_push($arr, array("id" => $user_id, "avatar" => base64_encode($avatar), "username" => $username, "pending" => $pending));
   }
 }
 
